49bb6109e17baca3bb9a16d9486d5c4fb8a54318
Haaragard <haaragard@gmail.com>
2019-05-08 21:25:31 -0300

    Novo projeto, Web Front 2 topper

--- src/app/categorias/categorias.module.ts
+++ src/app/categorias/categorias.module.ts
@@ -1,3 +1,6 @@
+import { RouterModule } from '@angular/router';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoriasPesquisaComponent } from './categorias-pesquisa/categorias-pesquisa.component';
@@ -9,10 +12,7 @@ import {TableModule} from 'primeng/table';
 import { CategoriasService } from './categorias.service';
 import {ToastModule} from 'primeng/toast';
 import { MessageService } from 'primeng/api';
-import { FormsModule } from '@angular/forms';
-import {ConfirmDialogModule} from 'primeng/confirmdialog';
 import { CategoriasCadastroComponent } from './categorias-cadastro/categorias-cadastro.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CategoriasPesquisaComponent, CategoriasCadastroComponent],
